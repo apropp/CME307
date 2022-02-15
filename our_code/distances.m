e091b18b73d5db6cd349f8cc287ebb893cc9d037
Adrienne Propp <apropp@rand.org>
2022-02-15 17:43:08 -0500

    Trying to figure out SOCP relaxation - not successful yet

--- our_code/distances.m
+++ our_code/distances.m
@@ -1,14 +1,9 @@
-function [Y_opt,X_opt] = SDPsnlsedumi_withoutSteepestDescent(PP,m,n,Radius,nf,degree)
-%SDPsnlsedumi_withoutSteepestDescent SDP relaxation using Sedumi package
-%   Solves sensor network localization using SDP relaxation. Steepest
-%   descent is not applied to the result.
+function [d] = distances(PP,m,n,Radius,nf,degree)
+%distances Compute distances between points within range.
+
 close all;
     PP=[PP(:,m+1:n) PP(:,1:m)]; % switches around the data so unknowns come first
     dd = n-m; % number of unknowns
-    Anchor = PP(:,dd+1:n); % anchor locations
-
-    e = [ones(dd,1); sum(PP(:,dd+1:n),2)]/sqrt(n);
-    C=1.4*nf*vec(e*e'-speye(dd+2,dd+2));
 
     b=zeros(3,1);
     a=sparse([zeros(1,dd) 1 0]');
@@ -20,7 +15,7 @@ close all;
     a=sparse([zeros(1,dd) 1 1]');
     A=[A vec(a*a')];
     b(3)=2;
-    %
+    
     % Compute pair-wise distances within R limit between known to unknown 
     % sensor-point and unknown to unknown within the radio range and set up
     % the constraint matrix.
@@ -47,36 +42,5 @@ close all;
                b(mtotal) = (dij(i,j))^2; 
             end;
         end;
-    end;
-
-    % Setup to call SEDUMI
-    mequal=mtotal
-    K.s=[dd+2];
-    K.s=[K.s ones(1,mtotal-3)];
-    pars.eps=1.e-3;
-    tic
-    A=[A; sparse([zeros(mtotal-3, 3) -eye(mtotal-3)])];
-    C=[C;ones(mtotal-3,1)];
-    pars.stepdif=0;
-    [x,y,info] = sedumi(A,b,C,K,pars);
-    Y_opt=mat(x(1:(dd+2)^2));
-	X_opt=Y_opt(dd+1:dd+2,1:dd);
-    Y_opt=Y_opt(1:dd,1:dd);
-    tvar=0;
-    sdpt=toc
-    % Plot the position estimations produced by the SDP solver
-    figure(1);
-    hold on ;
-    plot(PP(1,dd+1:n),PP(2,dd+1:n),'d');       
-    plot(X_opt(1,:),X_opt(2,:),'*r');
-    plot(PP(1,1:dd),PP(2,1:dd),'og');
-    for i=1:dd
-        plot([X_opt(1,i)  PP(1,i)] , [X_opt(2,i)  PP(2,i)]);
-    end;
-    hold off
-    % Compute the variance/trace and true error
-    for j=1:dd
-        tvar(j)=max(0,Y_opt(j,j)-(X_opt(:,j)'*X_opt(:,j)));
-        error(j)=norm(X_opt(:,j)-PP(:,j));
     end;
 end
