070dfe64597da9771944048f998fb7940276192f
Mpk <ishanik@stanford.edu>
2022-03-05 11:27:16 -0800

    updated some comments

--- our_code/part_1_code.m
+++ our_code/part_1_code.m
@@ -20,24 +20,24 @@ M = D <= radius;
 %% Q1
 
 % Vanilla SOCP
-% Z_1 = socp_solve(A, D, M, d, n_sensors, n_anchors);
-% % Vanilla SDP
-% Z_2 = sdp_solve(A, D, M, d, n_sensors, n_anchors);
-% % Vanilla SGD (NLL)
-% eps = 5*1e-3;
-% max_iters = 100; 
-% Z0 = rand(d*n_sensors, 1);
-% [Z_3, objs, errs] = ...
-%     nll_solve(A, D, M, d, n_sensors, n_anchors, X, Z0, eps, max_iters);
+Z_1 = socp_solve(A, D, M, d, n_sensors, n_anchors);
+% Vanilla SDP
+Z_2 = sdp_solve(A, D, M, d, n_sensors, n_anchors);
+% Vanilla SGD (NLL)
+eps = 5*1e-3;
+max_iters = 100; 
+Z0 = rand(d*n_sensors, 1);
+[Z_3, objs, errs] = ...
+    nll_solve(A, D, M, d, n_sensors, n_anchors, X, Z0, eps, max_iters);
 
 %% Q2
-% eps = 5*1e-3;
-% max_iters = 100; 
-% Z0 = reshape(Z_1, [d*n_sensors, 1]); 
-% [Z_3, objs_3, errs_3] = ...
-%     nll_solve(A, D, M, d, n_sensors, n_anchors, X, Z0, eps, max_iters);
+eps = 5*1e-3;
+max_iters = 100; 
+Z0 = reshape(Z_1, [d*n_sensors, 1]); 
+[Z_1_ref, objs_3, errs_3] = ...
+    nll_solve(A, D, M, d, n_sensors, n_anchors, X, Z0, eps, max_iters);
 
-%% Q3
+%% Q3 and Q4
 gamma = .05; 
 noise= normrnd(0, gamma, size(D)); 
 D_noisy = D + noise; 
@@ -48,26 +48,42 @@ Z_3_noise = noisy_socp_solve(A, D_noisy, M, d, n_sensors, n_anchors);
 eps = 5*1e-3;
 max_iters = 100; 
 Z0 = reshape(Z_2_noise, [d*n_sensors, 1]); 
-[Z_2_ref, objs_2_ref, errs_2_ref] = ...
+[Z_2_noise_ref, objs_2_noise_ref, errs_2_noise_ref] = ...
     nll_solve(A, D_noisy, M, d, n_sensors, ...
     n_anchors, X, Z0, eps, max_iters);
 
 eps = 5*1e-3;
 max_iters = 100; 
 Z0 = reshape(Z_3_noise, [d*n_sensors, 1]); 
-[Z_3_ref, objs_3_ref, errs_3_ref] = ...
+[Z_3_noise_ref, objs_3_noise_ref, errs_3_noise_ref] = ...
     nll_solve(A, D_noisy, M, d, n_sensors, ...
     n_anchors, X, Z0, eps, max_iters);
 
-% Plots 
-% evaluate_sensors(A, X, Z_1, n_sensors, n_anchors, 1)
-% evaluate_sensors(A, X, Z_2, n_sensors, n_anchors, 2)
-% evaluate_sensors(A, X, Z_3, n_sensors, n_anchors, 3)
-% visualize_descent(objs_3, errs_3, max_iters, 4)
+%% Plots 
 
-% evaluate_sensors(A, X, Z_2_noise, n_sensors, n_anchors, 5)
-% evaluate_sensors(A, X, Z_3_noise, n_sensors, n_anchors, 6)
+% Vanilla SOCP 
+evaluate_sensors(A, X, Z_1, n_sensors, n_anchors, 1)
+% Vanilla SDP 
+evaluate_sensors(A, X, Z_2, n_sensors, n_anchors, 2)
+% Vanilla SGD (NLL) 
+evaluate_sensors(A, X, Z_3, n_sensors, n_anchors, 3)
+visualize_descent(objs_3, errs_3, max_iters, 4)
 
-evaluate_sensors(A, X, Z_2_ref, n_sensors, n_anchors, 7)
-evaluate_sensors(A, X, Z_3_ref, n_sensors, n_anchors, 8)
+% Vanilla SOCP + SGD refinement (NLL) 
+evaluate_sensors(A, X, Z_1_ref, n_sensors, n_anchors, 3)
+visualize_descent(objs_3, errs_3, max_iters, 4)
+
+% Noisy SOCP
+evaluate_sensors(A, X, Z_2_noise, n_sensors, n_anchors, 5)
+% Noisy SDP
+evaluate_sensors(A, X, Z_3_noise, n_sensors, n_anchors, 6)
+
+% Noisy SOCP + SGD refinement
+evaluate_sensors(A, X, Z_2_noise_ref, n_sensors, n_anchors, 7)
+visualize_descent(objs_2_noise_ref, errs_2_noise_ref, max_iters, 4)
+% Noisy SDP + SGD refinement
+evaluate_sensors(A, X, Z_3_noise_ref, n_sensors, n_anchors, 8)
+visualize_descent(objs_3_noise_ref, errs_3_noise_ref, max_iters, 4)
+
+
 
