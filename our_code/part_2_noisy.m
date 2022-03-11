54da22559a531f0d75111dbcd1eb0a488f1e91a7
apropp <propp@stanford.edu>
2022-03-11 11:13:59 -0800

    Added radius experiment
    Radius experiment probably not necessary for report

--- our_code/part_2_noisy.m
+++ our_code/part_2_noisy.m
@@ -173,3 +173,97 @@ ylim([0 ymax+0.1*ymax])
 xlabel('SD Random Noise')
 ylabel('Inf-Norm Error')
 hold off
+
+%% Vary radii
+radii = 0.1:0.1:2;
+
+E2_norms = zeros(5, length(radii)); % store errors
+Inf_norms = zeros(5, length(radii)); % store errors
+
+rng(seeds(1));
+Z0 = rand(d*n_sensors, 1);
+A = rand(2,n_anchors); % n_anchors random points in coordinate plane
+X = rand(2,n_sensors); % n_sensors random points in the coordinate plane
+D = squareform(pdist([A,X]'));
+
+gamma = .05; 
+noise = normrnd(0, gamma, size(D)); 
+D_noisy = D + noise; 
+
+for i=1:length(radii)
+    radius = radii(i);
+    M = D <= radius;
+
+    Z_SOCP = noisy_socp_solve(A, D_noisy, M, d, n_sensors, n_anchors);
+    Z_SDP  = noisy_sdp_solve(A, D_noisy, M, d, n_sensors, n_anchors);
+    [Z_NLS, objs_NLS, errs_NLS, errsinf_NLS, k_NLS] = ...
+        nll_solve(A, D_noisy, M, d, n_sensors, n_anchors, X, Z0, eps, max_iters);
+
+    Z0_SOCP = reshape(Z_SOCP, [d*n_sensors, 1]); 
+    [Z_SOCP_NLS, objs_SOCP_NLS, errs_SOCP_NLS, errsinf_SOCP_NLS, k_SOCP_NLS] = ...
+        nll_solve(A, D_noisy, M, d, n_sensors, ...
+        n_anchors, X, Z0_SOCP, eps, max_iters);
+
+    Z0_SDP = reshape(Z_SDP, [d*n_sensors, 1]); 
+    [Z_SDP_NLS, objs_SDP_NLS, errs_SDP_NLS, errsinf_SDP_NLS, k_SDP_NLS] = ...
+        nll_solve(A, D_noisy, M, d, n_sensors, ...
+        n_anchors, X, Z0_SDP, eps, max_iters);
+
+    E2_norms(1,i) = norm(X-Z_SOCP);
+    E2_norms(2,i) = norm(X-Z_SDP);
+    E2_norms(3,i) = norm(X-Z_NLS);
+    E2_norms(4,i) = norm(X-Z_SOCP_NLS);
+    E2_norms(5,i) = norm(X-Z_SDP_NLS);
+    Inf_norms(1,i) = norm(X-Z_SOCP, inf);
+    Inf_norms(2,i) = norm(X-Z_SDP, inf);
+    Inf_norms(3,i) = norm(X-Z_NLS, inf);
+    Inf_norms(4,i) = norm(X-Z_SOCP_NLS, inf);
+    Inf_norms(5,i) = norm(X-Z_SDP_NLS, inf);
+end
+
+ymax = max(max(max(Inf_norms)),max(max(E2_norms)))
+
+figure
+subplot(1,2,1)
+set(0,'defaultTextInterpreter','latex'); %trying to set the default
+set(0,'defaultAxesFontSize',20)
+set(groot, 'DefaultLegendInterpreter', 'latex')
+set(gcf,'color','w');
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+
+plot(radii, E2_norms(1,:),'Color',[0 0.4470 0.7410], 'linewidth', 2)
+hold on
+plot(radii, E2_norms(2,:),'Color',[0.8500 0.3250 0.0980], 'linewidth', 2)
+hold on
+plot(radii, E2_norms(3,:),'Color',[0.3010 0.7450 0.9330], 'linewidth', 2)
+hold on
+plot(radii, E2_norms(4,:),'Color',[0.4940, 0.1840, 0.5560], 'linewidth', 2)
+hold on
+plot(radii, E2_norms(5,:),'Color',[0.9290, 0.6940, 0.1250], 'linewidth', 2)
+legend('SOCP', 'SDP', 'NLS', 'SOCP w NLS', 'SDP w NLS')
+xlabel('Radius')
+ylabel('2-Norm Error')
+ylim([0 ymax+0.1*ymax])
+hold off
+
+
+subplot(1,2,2)
+set(0,'defaultTextInterpreter','latex'); %trying to set the default
+set(0,'defaultAxesFontSize',20)
+set(groot, 'DefaultLegendInterpreter', 'latex')
+set(gcf,'color','w');
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+plot(radii, Inf_norms(1,:),'Color',[0 0.4470 0.7410], 'linewidth', 2)
+hold on
+plot(radii, Inf_norms(2,:),'Color',[0.8500 0.3250 0.0980], 'linewidth', 2)
+hold on
+plot(radii, Inf_norms(3,:),'Color',[0.3010 0.7450 0.9330], 'linewidth', 2)
+hold on
+plot(radii, Inf_norms(4,:),'Color',[0.4940, 0.1840, 0.5560], 'linewidth', 2)
+hold on
+plot(radii, Inf_norms(5,:),'Color',[0.9290, 0.6940, 0.1250], 'linewidth', 2)
+legend('SOCP', 'SDP', 'NLS', 'SOCP w NLS', 'SDP w NLS')
+ylim([0 ymax+0.1*ymax])
+xlabel('Radius')
+ylabel('Inf-Norm Error')
+hold off
